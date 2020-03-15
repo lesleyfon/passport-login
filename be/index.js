3c041cca25862f229be7492ee970ada71a7b9f1f
lesley <lesleybanadzem2017@gmail.com>
2020-03-15 12:12:08 -0700

    Added a facebook strategy to display page when we hit the auth/facebook endpont to aunthenticate user

--- be/index.js
+++ be/index.js
@@ -1,16 +1,43 @@
-require('dotenv').config()
-const express = require('express')
-const passport = require('passport')
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const passport = require("passport");
+const cors = require("cors");
 
-const FacebookStrategy = require('passport-facebook').Strategy;
+let user = {};
 
-const app = express()
-app.use(cors())
+const FacebookStrategy = require("passport-facebook").Strategy;
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: process.env.FACEBOOK_APP_ID,
+      clientSecret: process.env.FACEBOOK_APP_SECRET,
+      //Endpoint to rediect to Once we have a successful login
+      callbackURL: "http://localhost:5000/auth/facebook/callback"
+    },
+    function(accessToken, refreshToken, profile, done) {
+      user = {
+        ...profile
+      };
+      done(null, profile);
+    }
+  )
+);
+const app = express();
+app.use(cors());
 
-const port = 3000
+const port = 3000;
 
+app.get("/auth/facebook", passport.authenticate("facebook"));
 
-app.get('/', (req, res) => res.send('Welcome'))
+app.get( "/auth/facebook/callback", passport.authenticate("facebook", { successRedirect: "/profile", failureRedirect: "/login" }));
 
-app.listen(port, () => console.log(`App listening to http://localhost:${port}`))
+app.get("/profile", function(req, res){
+
+    res.json(user)
+})
+
+app.get("/", (req, res) => res.send("Welcome"));
+
+app.listen(port, () =>
+  console.log(`App listening to http://localhost:${port}`)
+);
